b649af4c7edfce77997d2926710538a47d2d556f
Egor <egorbgtrvch@gmail.com>
2022-04-30 22:35:15 +0300

    Поправил форму

--- templates/get_form.py
+++ templates/get_form.py
@@ -1,24 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
-from wtforms.validators import Length
 
 
 class GetForm(FlaskForm):
     base_image = FileField(
         'Base Image',
-        [
+        validators=[
             FileRequired(),
-            FileAllowed(['.jpg', '.png'],
-                        message='Not a valid file extension.')
+            FileAllowed(['jpg', 'png'],
+                        'Not a valid file extension.')
         ]
     )
     compared_image = FileField(
         'Compared Image',
-        [
+        validators=[
             FileRequired(),
-            FileAllowed(['.jpg', '.png'],
-                        message='Not a valid file extension.')
+            FileAllowed(['jpg', 'png'],
+                        'Not a valid file extension.')
         ]
     )
     submit = SubmitField('Submit')

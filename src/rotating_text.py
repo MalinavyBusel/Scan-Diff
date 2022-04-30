bbce30718d8c442e75b371a3cff4a1242b09d635
Egor <egorbgtrvch@gmail.com>
2022-04-30 09:41:28 +0300

    Добавил фильтр для повышения качества распознавания

--- src/rotating_text.py
+++ src/rotating_text.py
@@ -1,25 +1,35 @@
 import pytesseract
 import tempfile
 
-from PIL import Image
+from PIL import Image, ImageFilter
 from pytesseract import Output
 
 
-def set_image_dpi(image: Image) -> Image:
+def set_dpi_and_filter(image: Image) -> Image:
     """
-    Rescaling image to 300dpi without resizing
+    Rescaling image to 300dpi and filter it to improve the quality of recognition
     :param image: A PIL or cv2 image
     :return: A rescaled image
     """
-    image_resize = image
+    pytess_image = image
+
+    pytess_image = pytess_image.filter(ImageFilter.SHARPEN)
+
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
     temp_filename = temp_file.name
-    image_resize.save(temp_filename, dpi=(300, 300))
+    pytess_image.save(temp_filename, dpi=(300, 300))
     return temp_filename
 
 
 def get_rotation_angle(input_image: Image) -> int:
+    """
+    Get the image, tries to recognize the text and returns the number of degrees to rotate
+    Since the lang is not specified, its better not to use for hieroglyphs
+    :param input_image: A PIL or cv2 image
+    :return: A number of degrees
+    """
     image_ = set_image_dpi(input_image)
+    # не cтавил языки в image_to_osd, тк так результаты точнее
     results = pytesseract.image_to_osd(image_,
                                        config='--psm 0 -c min_characters_to_try=5',
                                        output_type=Output.DICT)
